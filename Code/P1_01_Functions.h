76b09d14562353d646a1ab8da630dc240497c44d
itsSuwy <diego.velazquez.2006@outlook.es>
2026-04-25 21:16:48 -0600

    refactor: Modify the order of the functions including a tag for recursives and delete dead functions

--- Code/P1_01_Functions.h
+++ Code/P1_01_Functions.h
@@ -40,40 +40,34 @@ struct pila {
 };
 
 int determinarHilos(void);
-void Crear_CPU(struct cpu *CPU, int hilos);
+void Crear_CPU(struct cpu *CPU, int hilos); // Recursiva
 struct thread *Crear_Hilos(int hilos);
-void impresion(struct thread *Hilo);
+char *nombre(void);
+bool designar_importancia(void);;
+struct process *proceso_empaquetado(char *name, bool urgency);
+void cola_global(struct cpu *CPU, struct process *proceso_nuevo);
+struct process *proceso_final(struct process *proceso_cola); // Recursiva
+int repeticion(void);
+void carga_proceso(struct cpu *CPU);
+void acceso_CPU(struct cpu *CPU); // Recursiva
+struct process *extraer_proceso_inicial(struct stack *pendientes);
+struct thread *extraer_hilo(struct thread *Hilo, struct thread *Hilo_respaldo); // Recursiva
+void asignar_Proceso(struct thread *Hilo, struct process *proceso);
+void proceso_urgente(struct thread *Hilo, struct process *proceso);
+void proceso_comun(struct thread *Hilo, struct process *proceso);
+void impresion(struct thread *Hilo); // Recursiva
+void visualizar_procesos(struct thread *hilo); // Recursiva
+void extraer_proceso(struct process *process); // Recursiva
+struct pila *crear_pila(struct cpu *CPU);
+void extraccion_hilos(struct thread *hilo, struct pila *output, struct process *proceso); // Recursiva
+void push(struct thread *hilo, struct pila *output);
+void actualizar_hilo(struct thread *hilo);
+void impresion_pila(struct process *proceso); // Recursiva
+void subir_archivo(struct process *proceso);
+void procesar_archivo(struct process *proceso, FILE *fp); // Recursiva
 int procesado_de_entrada(char input);
 void limpiando_buffer(void);
 void ordenar_CPU(struct cpu *CPU); // WIP
 void ordenar_CPU_auxiliar(struct cpu *CPU, struct thread *hilo); // WIP
-char *nombre(void);
-bool designar_importancia(void);
-struct process *proceso_empaquetado(char *name, bool urgency);
-void asignar_hilo(struct thread *hilo_original, struct thread *hilo_auxiliar, struct process *process);
-int busqueda_hilo_libre(struct thread *hilo, struct process *process);
-struct process *busqueda_proceso_final(struct process *process);
-void asignacion_proceso(struct thread *hilo, struct process *process);
-struct thread *hilo_libre(struct thread *hilo_actual, struct thread *hilo_menor);
-void asignacion_proceso_ocupado(struct thread *hilo,struct process *process);
-void visualizar_procesos(struct thread *hilo);
-void extraer_proceso(struct process *process);
-struct pila *crear_pila(struct cpu *CPU);
-void extraccion_hilos(struct thread *hilo, struct pila *output, struct process *proceso);
-void push(struct thread *hilo, struct pila *output);
-void impresion_pila(struct process *proceso);
-void subir_archivo(struct process *proceso);
-void procesar_archivo(struct process *proceso, FILE *fp);
-void actualizar_hilo(struct thread *hilo);
-void cola_global(struct cpu *CPU, struct process *proceso_nuevo);
-struct process *proceso_final(struct process *proceso_cola);
-void carga_proceso(struct cpu *CPU);
-int repeticion(void);
-void acceso_CPU(struct cpu *CPU);
-struct process *extraer_proceso_inicial(struct stack *pendientes);
-struct thread *extraer_hilo(struct thread *Hilo, struct thread *Hilo_respaldo);
-void asignar_Proceso(struct thread *Hilo, struct process *proceso);
-void proceso_urgente(struct thread *Hilo, struct process *proceso);
-void proceso_comun(struct thread *Hilo, struct process *proceso);
 
 #endif //THREAD_POOL_OS_SIMULATOR_P1_01_FUNCTIONS_H
